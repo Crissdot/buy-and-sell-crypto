86996f195372d769e53926d4722ddf91af4fec9b
crissdot <cris-2-11-2001@hotmail.com>
2022-07-17 12:27:41 -0500

    add fund account

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 import './Header.css';
 
 function Header(props) {
@@ -9,6 +9,10 @@ function Header(props) {
                 <NavLink className="header-nav__home" to='/'><span>Inicio</span></NavLink>
                 <NavLink className="header-nav__favs" to='/favoritos'><span>Favoritos</span></NavLink>
             </nav>
+            <button className="header__fund" type="button">
+                <span>Fondear Cuenta</span>
+                <svg className="w-6 h-6" fill="var(--primary-color)" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z" /><path fillRule="evenodd" d="M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z" clipRule="evenodd" /></svg>
+            </button>
         </header>
     );
 }
